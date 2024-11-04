7edd1db44450963f2c17536587ec6bf7ed24c41d
sardor0990 <sardorzufarov98@gmail.com>
2024-11-04 22:11:17 +0500

    created file [+]

---
+++ src/main/java/org/example/javaspringboot_security_online_course_jlkesh/JavaSpringBootSecurityOnlineCourseJlkeshApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaspringboot_security_online_course_jlkesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootSecurityOnlineCourseJlkeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootSecurityOnlineCourseJlkeshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaspringboot_security_online_course_jlkesh/JavaSpringBootSecurityOnlineCourseJlkeshApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaspringboot_security_online_course_jlkesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootSecurityOnlineCourseJlkeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
